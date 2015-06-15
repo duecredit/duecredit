e9d06173dbf3e8fec04692c4bbbb85e151eeb8cd
Yaroslav Halchenko <debian@onerussian.com>
2015-06-14 14:32:48 -1000

    ENH+BF: save and load pickle in binary mode, more meaningful ERRORED msg

--- duecredit/io.py
+++ duecredit/io.py
@@ -134,10 +134,9 @@ def format_bibtex(bibtex_entry, style='harvard1'):
             f.write(bibtex)
         try:
             bib_source = cpBibTeX(fname)
-        except:
+        except Exception as e:
             lgr.error("Failed to process BibTeX file %s" % fname)
-            os.system("cp %s /tmp/failed.bib" % fname)
-            return "ERRORED"
+            return "ERRORED: %s" % str(e)
         bib_style = cp.CitationStylesStyle(style, validate=False)
         # TODO: specify which kind of formatter we want
         bibliography = cp.CitationStylesBibliography(bib_style, bib_source,
@@ -164,7 +163,7 @@ class PickleOutput(object):
 
     @classmethod
     def load(cls, filename=DUECREDIT_FILE):
-        with open(filename) as f:
+        with open(filename, 'rb') as f:
             return pickle.load(f)
 
 class BibTeXOutput(object):  # TODO some parent class to do what...?
