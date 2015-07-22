2ab59561ea590fc7159e6a9f170e98331c58bbf4
Matteo Visconti dOC <matteo.visconti.gr@dartmouth.edu>
2015-07-22 12:32:17 -0400

    Add more references for scipy

--- duecredit/injections/mod_scipy.py
+++ duecredit/injections/mod_scipy.py
@@ -27,7 +27,94 @@ def inject(injector):
       note = {[Online; accessed 2015-07-13]}
     }"""), description="Scientific tools library")
 
-    injector.add('scipy.cluster.hierarchy', 'ward', BibTeX("""
+    # scipy.cluster.hierarchy general references
+    # TODO: we should allow to pass a list of entries
+    injector.add('scipy.cluster.hierarchy', None, BibTeX("""
+    @article{johnson1967hierarchical,
+        title={Hierarchical clustering schemes},
+        author={Johnson, Stephen C},
+        journal={Psychometrika},
+        volume={32},
+        number={3},
+        pages={241--254},
+        year={1967},
+        publisher={Springer}
+    }"""), min_version='v0.4.3',
+                 description="Global references for the module")
+
+    injector.add('scipy.cluster.hierarchy', None, BibTeX("""
+    @article{sneath1962numerical,
+        title={Numerical taxonomy},
+        author={Sneath, Peter HA and Sokal, Robert R},
+        journal={Nature},
+        volume={193},
+        number={4818},
+        pages={855--860},
+        year={1962},
+        publisher={Nature Publishing Group}
+    }"""), description="Global references for the module",
+                 min_version='v0.4.3')
+
+    injector.add('scipy.cluster.hierarchy', None, BibTeX("""
+    @article{batagelj1995comparing,
+        title={Comparing resemblance measures},
+        author={Batagelj, Vladimir and Bren, Matevz},
+        journal={Journal of classification},
+        volume={12},
+        number={1},
+        pages={73--90},
+        year={1995},
+        publisher={Springer}
+    }"""), description="Global references for the module",
+                 min_version='v0.4.3')
+
+    injector.add('scipy.cluster.hierarchy', None, BibTeX("""
+    @book{sokal1958statistical,
+        author = {Sokal, R R and Michener, C D and {University of Kansas}},
+        title = {{A Statistical Method for Evaluating Systematic Relationships}},
+        publisher = {University of Kansas},
+        year = {1958},
+        series = {University of Kansas science bulletin}
+    }"""), description="Global references for the module",
+                 min_version='v0.4.3')
+
+    injector.add('scipy.cluster.hierarchy', None, BibTeX("""
+    @article{edelbrock1979mixture,
+        title={Mixture model tests of hierarchical clustering algorithms:
+            the problem of classifying everybody},
+        author={Edelbrock, Craig},
+        journal={Multivariate Behavioral Research},
+        volume={14},
+        number={3},
+        pages={367--384},
+        year={1979},
+        publisher={Taylor \& Francis}
+    }"""), description="Global references for the module",
+                 min_version='v0.4.3')
+
+    injector.add('scipy.cluster.hierarchy', None, BibTeX("""
+    @book{jain1988algorithms,
+        title={Algorithms for clustering data},
+        author={Jain, Anil K and Dubes, Richard C},
+        year={1988},
+        publisher={Prentice-Hall, Inc.}
+    }"""), description="Global references for the module",
+                 min_version='v0.4.3')
+
+    injector.add('scipy.cluster.hierarchy', None, BibTeX("""
+    @article{fisher1936use,
+        title={The use of multiple measurements in taxonomic problems},
+        author={Fisher, Ronald A},
+        journal={Annals of eugenics},
+        volume={7},
+        number={2},
+        pages={179--188},
+        year={1936},
+        publisher={Wiley Online Library}
+    }"""), description="Global references for the module",
+                 min_version='v0.4.3')
+    # Here options for linkage
+    injector.add('scipy.cluster.hierarchy', 'linkage', BibTeX("""
     @article{ward1963hierarchical,
         title={Hierarchical grouping to optimize an objective function},
         author={Ward Jr, Joe H},
@@ -37,9 +124,11 @@ def inject(injector):
         pages={236--244},
         year={1963},
         publisher={Taylor \& Francis}
-    }"""), description="Ward hierarchical clustering", min_version='v0.4.3')
+    }"""), conditions={(1, 'method'): {'ward'}},
+                 description="Ward hierarchical clustering",
+                 min_version='v0.4.3')
 
-    injector.add('scipy.cluster.hierarchy', 'single', BibTeX("""
+    injector.add('scipy.cluster.hierarchy', 'linkage', BibTeX("""
     @article{gower1969minimum,
         title={Minimum spanning trees and single linkage cluster analysis},
         author={Gower, John C and Ross, GJS},
@@ -47,9 +136,10 @@ def inject(injector):
         pages={54--64},
         year={1969},
         publisher={JSTOR}
-    }"""), description="Single linkage hierarchical clustering")
+    }"""), conditions={(1, 'method'): {'single', 'DC_DEFAULT'}},
+                 description="Single linkage hierarchical clustering")
 
-    injector.add('scipy.cluster.hierarchy', 'single', BibTeX("""
+    injector.add('scipy.cluster.hierarchy', 'linkage', BibTeX("""
     @article{sibson1973slink,
         title={SLINK: an optimally efficient algorithm for the single-link
         cluster method},
@@ -60,4 +150,5 @@ def inject(injector):
         pages={30--34},
         year={1973},
         publisher={Br Computer Soc}
-    }"""), description="Single linkage hierarchical clustering")
+    }"""), conditions={(1, 'method'): {'single', 'DC_DEFAULT'}},
+                 description="Single linkage hierarchical clustering")
