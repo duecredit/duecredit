ca7b64c1ffe216482e035cac357593833c3dd310
Yaroslav Halchenko <debian@onerussian.com>
2015-07-24 15:51:42 -0400

    ENH: snippet from PyMVPA for backtracing in logs

--- duecredit/log.py
+++ duecredit/log.py
@@ -15,6 +15,67 @@ from .utils import is_interactive
 
 __all__ = ['ColorFormatter', 'log']
 
+# Snippets from traceback borrowed from PyMVPA upstream/2.4.0-39-g69ad545  MIT license
+
+import traceback
+import re
+from os.path import basename, dirname
+
+def mbasename(s):
+    """Custom function to include directory name if filename is too common
+
+    Also strip .py at the end
+    """
+    base = basename(s)
+    if base.endswith('.py'):
+        base = base[:-3]
+    if base in set(['base', '__init__']):
+        base = basename(dirname(s)) + '.' + base
+    return base
+
+class TraceBack(object):
+    """Customized traceback to be included in debug messages
+    """
+
+    def __init__(self, collide=False):
+        """Initialize TrackBack metric
+
+        Parameters
+        ----------
+        collide : bool
+          if True then prefix common with previous invocation gets
+          replaced with ...
+        """
+        self.__prev = ""
+        self.__collide = collide
+
+    def __call__(self):
+        ftb = traceback.extract_stack(limit=100)[:-2]
+        entries = [[mbasename(x[0]), str(x[1])] for x in ftb]
+        entries = [ e for e in entries if e[0] != 'unittest' ]
+
+        # lets make it more consize
+        entries_out = [entries[0]]
+        for entry in entries[1:]:
+            if entry[0] == entries_out[-1][0]:
+                entries_out[-1][1] += ',%s' % entry[1]
+            else:
+                entries_out.append(entry)
+        sftb = '>'.join(['%s:%s' % (mbasename(x[0]),
+                                    x[1]) for x in entries_out])
+        if self.__collide:
+            # lets remove part which is common with previous invocation
+            prev_next = sftb
+            common_prefix = os.path.commonprefix((self.__prev, sftb))
+            common_prefix2 = re.sub('>[^>]*$', '', common_prefix)
+
+            if common_prefix2 != "":
+                sftb = '...' + sftb[len(common_prefix2):]
+            self.__prev = prev_next
+
+        return sftb
+
+
 # Recipe from http://stackoverflow.com/questions/384076/how-can-i-color-python-logging-output
 # by Brandon Thomson
 # Adjusted for automagic determination either coloring is needed and
