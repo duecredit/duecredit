8aabff5da15f5a94da9c638933828b10ad471120
Yaroslav Halchenko <debian@onerussian.com>
2015-08-05 23:51:03 -0400

    ENH: comment out explicit import of duecredit in example and added a comment

--- examples/example_scipy.py
+++ examples/example_scipy.py
@@ -1,4 +1,8 @@
-import duecredit  # Just to enable duecredit
+# A tiny analysis script to demostrate duecredit
+#
+# Import of duecredit is not necessary if you just run this script with
+# python -m duecredit
+# import duecredit  # Just to enable duecredit
 from scipy.cluster.hierarchy import linkage
 from scipy.spatial.distance import pdist
 from sklearn.datasets import make_blobs
