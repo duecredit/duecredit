5bd03608b14384ac6268fe8e7e8d1c035892d9fa
Yaroslav Halchenko <debian@onerussian.com>
2015-09-24 16:07:38 -0400

    ENH: decorate .train of the mdp nodes instead of constructor, adjust descriptions

--- duecredit/injections/mod_mdp.py
+++ duecredit/injections/mod_mdp.py
@@ -24,14 +24,14 @@ injector.add('', None, Doi(''),
 
 def inject(injector):
     injector.add('mdp', None, Doi('10.3389/neuro.11.008.2008'),
-                 description="Modular toolkit for Data Processing (MDP): a Python data processing frame work.",
+                 description="Modular toolkit for Data Processing (MDP): a Python data processing framework",
                  tags=['implementation'])
 
-    injector.add('mdp.nodes', 'PCANode', Doi('10.1007/b98835'),
-                 description="Filter the input data through the most significatives of its principal components.",
+    injector.add('mdp.nodes', 'PCANode.train', Doi('10.1007/b98835'),
+                 description="Principal Component Analysis (and filtering)",
                  tags=['implementation'])
 
-    injector.add('mdp.nodes', 'NIPALSNode', BibTeX("""
+    injector.add('mdp.nodes', 'NIPALSNode.train', BibTeX("""
         @incollection{Word1966,
             author={Wold, H.},
             title={Nonlinear estimation by iterative least squares procedures.},
@@ -41,18 +41,18 @@ def inject(injector):
             editor={David, F.},
             pages={411-444},
             }
-        """), description="Perform Principal Component Analysis using the NIPALS algorithm.", 
+        """), description="Principal Component Analysis using the NIPALS algorithm.",
         tags=['edu'])
 
-    injector.add('mdp.nodes', 'FastICANode', Doi('10.1109/72.761722'),
-                 description="Perform Independent Component Analysis using the FastICA algorithm",
+    injector.add('mdp.nodes', 'FastICANode.train', Doi('10.1109/72.761722'),
+                 description="Independent Component Analysis using the FastICA algorithm",
                  tags=['implementation'])
 
-    injector.add('mdp.nodes', 'CuBICANode', Doi('10.1109/TSP.2004.826173'),
-                 description='Perform Independent Component Analysis using the CuBICA algorithm.',
+    injector.add('mdp.nodes', 'CuBICANode.train', Doi('10.1109/TSP.2004.826173'),
+                 description='Independent Component Analysis using the CuBICA algorithm.',
                  tags=['implementation'])
     
-    injector.add('mdp.nodes', 'NIPALSNode', BibTeX("""
+    injector.add('mdp.nodes', 'NIPALSNode.train', BibTeX("""
         @conference{ZieheMuller1998,
             author={Ziehe, Andreas and Muller, Klaus-Robert},
             title={TDSEP an efficient algorithm for blind separation using time structure.},
@@ -61,32 +61,32 @@ def inject(injector):
             editor={Niklasson, L, Boden, M, and Ziemke, T},
             publisher={ICANN}
             }
-        """), description="Perform Independent Component Analysis using the TDSEP algorithm.", 
+        """), description='Independent Component Analysis using the TDSEP algorithm',
         tags=['edu'])
 
 
-    injector.add('mdp.nodes', 'JADENode', Doi('10.1049/ip-f-2.1993.0054'),
-                    description='Perform Independent Component Analysis using the JADE algorithm.',
+    injector.add('mdp.nodes', 'JADENode.train', Doi('10.1049/ip-f-2.1993.0054'),
+                    description='Independent Component Analysis using the JADE algorithm',
                     tags=['implementation'])
-    injector.add('mdp.nodes', 'JADENode', Doi('10.1162/089976699300016863'),
-                    description='Perform Independent Component Analysis using the JADE algorithm.',
-                    tags=['implementation'])
-
-    injector.add('mdp.nodes', 'SFANode', Doi('10.1162/089976602317318938'),
-                    description='Extract the slowly varying components from the input data.',
+    injector.add('mdp.nodes', 'JADENode.train', Doi('10.1162/089976699300016863'),
+                    description='Independent Component Analysis using the JADE algorithm',
                     tags=['implementation'])
 
-    injector.add('mdp.nodes', 'SFA2Node', Doi('10.1162/089976602317318938'),
-                    description='Get an input signal, expand it in the space of inhomogeneous polynomials of degree 2 and extract its slowly varying components.',
+    injector.add('mdp.nodes', 'SFANode.train', Doi('10.1162/089976602317318938'),
+                    description='Slow Feature Analysis',
+                    tags=['implementation'])
+
+    injector.add('mdp.nodes', 'SFA2Node.train', Doi('10.1162/089976602317318938'),
+                    description='Slow Feature Analysis (via the space of inhomogeneous polynomials)',
                     tags=['implementation'])
     
 
-    injector.add('mdp.nodes', 'ISFANode', Doi('10.1007/978-3-540-30110-3_94'),
-                    description='Perform Independent Slow Feature Analysis on the input data.',
+    injector.add('mdp.nodes', 'ISFANode.train', Doi('10.1007/978-3-540-30110-3_94'),
+                    description='Independent Slow Feature Analysis',
                     tags=['implementation'])
 
  
-    injector.add('mdp.nodes', 'XSFANode', BibTeX("""
+    injector.add('mdp.nodes', 'XSFANode.train', BibTeX("""
         @article{SprekelerZitoWiskott2009,
             author={Sprekeler, H., Zito, T., and Wiskott, L.},
             title={An Extension of Slow Feature Analysis for Nonlinear Blind Source Separation.},
@@ -95,18 +95,18 @@ def inject(injector):
             volume={15},
             pages={921-947},
             }
-        """), description="Perform Non-linear Blind Source Separation using Slow Feature Analysis.", 
+        """), description="Non-linear Blind Source Separation using Slow Feature Analysis",
         tags=['edu'])
 
-    injector.add('mdp.nodes', 'FDANode._train', BibTeX("""
-        @book{Nishop2011,
+    injector.add('mdp.nodes', 'FDANode.train', BibTeX("""
+        @book{Bishop2011,
             author={Christopher M. Bishop},
             title={Neural Networks for Pattern Recognition},
             publisher={Oxford University Press, Inc}
             year={2011},
             pages={105-112},
             }
-        """), description="Perform a (generalized) Fisher Discriminant Analysis of its input.", 
+        """), description="(generalized) Fisher Discriminant Analysis",
         tags=['edu'])
 
     # etc...
