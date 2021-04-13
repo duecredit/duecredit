f33259f63714937e08085ff234ac04896ba1586f
Yaroslav Halchenko <debian@onerussian.com>
2021-04-13 13:45:55 -0400

    Drop python2 support and CI, add 3.8 into CI

--- setup.py
+++ setup.py
@@ -105,7 +105,6 @@ setup(
         'License :: OSI Approved :: BSD License',
         'Operating System :: OS Independent',
         'Programming Language :: Python',
-        'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3',
         'Topic :: Documentation',
         'Topic :: Software Development :: Documentation',
