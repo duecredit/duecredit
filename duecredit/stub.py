e36c3f6b4d9adc70c3b9550bda1158cd954c5c4c
John T. Wodder II <git@varonathe.org>
2021-06-08 09:03:31 -0400

    Tell LGTM to ignore unused imports in stub.py

--- duecredit/stub.py
+++ duecredit/stub.py
@@ -52,7 +52,7 @@ def _donothing_func(*args, **kwargs):
 
 
 try:
-    from duecredit import due, BibTeX, Doi, Url, Text
+    from duecredit import due, BibTeX, Doi, Url, Text  # lgtm [py/unused-import]
     if 'due' in locals() and not hasattr(due, 'cite'):
         raise RuntimeError(
             "Imported due lacks .cite. DueCredit is now disabled")
